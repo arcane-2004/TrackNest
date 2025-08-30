3144b5fa0290b87c7d0b11782ed0cd15e365e848
arcane_2004 <sumitkp153@gmail.com>
2025-08-30 16:45:09 +0530

    updated sign in page with validation

--- Frontend/src/components/Signin.jsx
+++ Frontend/src/components/Signin.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import * as Yup from "yup"
+import { Form, Formik } from "formik"
+import { useState } from "react";
+
 import {
   IconBrandGithub,
   IconBrandGoogle,
@@ -10,64 +14,118 @@ import {
 } from "@tabler/icons-react";
 
 export default function SignupFormDemo() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
+
+  const [visibility, setVisibility] = useState(false);
+
+  const visibilityHandle = () => {
+    setVisibility(!visibility)
+  }
+
+  const initialStates = {
+    email: '',
+    password: ''
+  }
+
+  const validtionSchema = Yup.object({
+    email: Yup.string().email("Invaild email").required('email is required'),
+    password: Yup.string().required('password is requried')
+  })
+
+
+
+  const handleSubmit = (values) => {
+    console.log(values);
+    
   };
   return (
-    <div
-      className=" mx-auto w-full max-w-md rounded-none p-4 md:rounded-2xl md:p-8">
-      <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
-      </h2>
-      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-        Login to aceternity if you can because we don&apos;t have a login flow
-        yet
-      </p>
-      <form className="my-8" onSubmit={handleSubmit}>
-        
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder=" " type="email" />
-        </LabelInputContainer>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder=" " type="password" />
-        </LabelInputContainer>
-        
+    <div>
+      <div
+        className=" mx-auto mr-20 w-full max-w-md rounded-none p-4 md:rounded-2xl md:p-8">
+        <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
+          Welcome to Aceternity
+        </h2>
+        <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+          Login to aceternity if you can because we don&apos;t have a login flow
+          yet
+        </p>
 
-        <button
-          className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
-          type="submit">
-          Sign up &rarr;
-          <BottomGradient />
-        </button>
+      </div>
 
-        <div
-          className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+      <Formik onSubmit={handleSubmit} validationSchema={validtionSchema} initialValues={initialStates}>
+        {({ handleBlur, handleChange, values, touched, errors }) =>
+          <div className="mx-auto mr-20 w-full max-w-md rounded-none p-4 md:rounded-2xl md:p-8 ">
+            <Form className="my-8">
+              <LabelInputContainer className="mb-4">
+                <Label htmlFor="email">Email Address</Label>
+                <Input
+                
+                  name='email'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.email && Boolean(errors.email)}
+                  helperText={touched.email && errors.email}
+                  id="email" placeholder=" " type="text"
+                />
 
-        <div className="flex flex-col space-y-4">
-          <button
-            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-            type="submit">
-            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-sm text-neutral-700 dark:text-neutral-300">
-              GitHub
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-            type="submit">
-            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-sm text-neutral-700 dark:text-neutral-300">
-              Google
-            </span>
-            <BottomGradient />
-          </button>
-         
-        </div>
-      </form>
+                {touched.email && errors.email && (
+                  <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+                )}
+              </LabelInputContainer>
+
+              <LabelInputContainer className="mb-4 relative">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  placeholder=" "
+                  type={visibility ? "text" : "password"}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+
+                <button
+                  onClick={visibilityHandle}
+                  className="absolute right-3 top-7.5 text-white hover:cursor-pointer">
+                  {visibility ? <i className="ri-eye-off-line"></i> : <i className="ri-eye-line"></i>}
+                </button>
+
+                {touched.password && errors.password && (
+                  <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+                )}
+              </LabelInputContainer>
+
+
+              <button
+                className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset] hover:cursor-pointer"
+
+                type="submit">
+                Sign in &rarr;
+                <BottomGradient />
+              </button>
+              <button className="text-gray-800 font-medium text-sm mt-1.5 hover:cursor-pointer" type="text" >
+                Forget password &rarr;
+                <BottomGradient />
+              </button>
+
+              <div
+                className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+
+              <div className="flex flex-col space-y-4">
+                <button
+                  className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626] hover:cursor-pointer"
+                  type="submit">
+                  <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+                  <span className="text-sm text-neutral-700 dark:text-neutral-300">
+                    Google
+                  </span>
+                  <BottomGradient />
+                </button>
+
+              </div>
+            </Form>
+          </div>
+        }
+
+      </Formik>
     </div>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
+import 'remixicon/fonts/remixicon.css'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Toaster />
       <App />
     </BrowserRouter>
   </StrictMode>,
