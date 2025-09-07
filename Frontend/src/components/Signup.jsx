fe015e5a31676bd6b5330e9d8517d32f659d3cd0
arcane_2004 <sumitkp153@gmail.com>
2025-09-07 12:10:12 +0530

    added  google signup to sign up page

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -51,8 +51,15 @@ export default function SignupFormDemo() {
       console.log(data);
 
     }
-
   };
+
+
+  const handleGoogleSignIn = () => {
+    window.location.href = `${import.meta.env.VITE_BASE_URL}/user/auth/google`
+  }
+
+
+
   return (
     <div
       className="mx-auto w-full max-w-md rounded-none  p-4 md:rounded-2xl md:p-8 ">
@@ -127,7 +134,7 @@ export default function SignupFormDemo() {
 
 
             <button
-              className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+              className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset] hover:cursor-pointer"
               type="submit">
               Sign up &rarr;
               <BottomGradient />
@@ -139,8 +146,9 @@ export default function SignupFormDemo() {
             <div className="flex flex-col space-y-4">
 
               <button
-                className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-                type="button">
+                className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626] hover:cursor-pointer"
+                type="button"
+                onClick={handleGoogleSignIn}>
                 <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
                 <span className="text-sm text-neutral-700 dark:text-neutral-300">
                   Google
